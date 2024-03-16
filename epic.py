342f45ed25b2b5fc68b424867c301f8a67b05234
arsMM22 <129196828+arsMM22@users.noreply.github.com>
2024-03-16 15:11:46 +0300

    Update epic.py

--- epic.py
+++ epic.py
@@ -5,7 +5,7 @@ from dowloand_image import download_image
 from dotenv import load_dotenv
 
 
-def epic_image():
+def epic_images():
     nasa_token = os.environ['NASA_TOKEN']
     url = 'https://api.nasa.gov/EPIC/api/natural/image'
     params = {
@@ -13,10 +13,10 @@ def epic_image():
         "count": 2
     }
     response = requests.get(url, params=params)
-    for epic_image in response.json():
-        epic_date = epic_image['date']
+    for epic_images in response.json():
+        epic_date = epic_images['date']
         epic_date = datetime.fromisoformat(epic_date).strftime("%Y/%m/%d")
-        epic_name = epic_image['image']
+        epic_name = epic_images['image']
         epic_url = f"https://api.nasa.gov/EPIC/archive/natural/{epic_date}/png/{epic_name}.png"
         epic_path = os.path.join('images', f'{epic_name}.png')
         download_image(epic_url, epic_path)
