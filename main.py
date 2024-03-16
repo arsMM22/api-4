eeef6cef6c94b779a9d830e7ed57e29d4216032b
arsMM22 <129196828+arsMM22@users.noreply.github.com>
2024-03-16 15:30:24 +0300

    связал числа с кодом

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 
 def main():
+    count - 14400
     load_dotenv()
     tg_token = os.environ['TELEGRAM_TOKEN']
     bot = telegram.Bot(token=tg_token)
@@ -19,7 +20,7 @@ def main():
             path = os.path.join("images", file)
             with open(path, "rb") as f:
                 bot.send_document(chat_id="@spacebot1234", document=f)
-            sleep(14400)
+            sleep(count)
 
 
 if __name__ == "__main__":
