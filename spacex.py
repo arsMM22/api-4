69b398953677d21f445b17d766686c0c6db07421
arsMM22 <129196828+arsMM22@users.noreply.github.com>
2024-03-16 15:59:55 +0300

    добавил argparse

--- spacex.py
+++ spacex.py
@@ -2,8 +2,8 @@ import requests
 from dowloand_image import download_image
 
 
-def fetch_spacex_last_launch():
-    url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
+def fetch_spacex_last_launch(launch_id):
+    url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(url)
     response.raise_for_status()
     for image_number, image in enumerate(
@@ -12,7 +12,17 @@ def fetch_spacex_last_launch():
 
 
 def main():
-    fetch_spacex_last_launch()
+    parser = argparse.ArgumentParser(
+        description='Эта программа позволит вам загрузить фотографии с запуска SpaceX.'
+    )
+    parser.add_argument(
+        '--id',
+        dest='launch_id',
+        default="5eb87d47ffd86e000604b38a",
+        help='Укажите ID запуска SpaceX, с которого можно загрузить фотографии.'
+    )
+    args = parser.parse_args()
+    fetch_spacex_last_launch(args.launch_id)
 
 
 if __name__ == "__main__":
