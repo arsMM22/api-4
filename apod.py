50fe7f978b35ee02e4ab65c208d31bac2ce674dd
arsMM22 <129196828+arsMM22@users.noreply.github.com>
2024-03-16 15:15:14 +0300

    изолировал функцию от внешнего окружения

--- apod.py
+++ apod.py
@@ -13,7 +13,7 @@ def get_extension_link(link):
     return extension, file_name
 
 
-def fetch_apond_images():
+def fetch_apond_images(nasa_token):
     url = 'https://api.nasa.gov/planetary/apod'
     params = {
         "api_key": nasa_token,
