6ebd3c5e401579afb14f226f43657758d0c75032
arsMM22 <129196828+arsMM22@users.noreply.github.com>
2024-03-16 15:46:42 +0300

    добавил секрет

--- upload_images.py
+++ upload_images.py
@@ -9,17 +9,16 @@ from dotenv import load_dotenv
 def main():
     sleep - 14400
     load_dotenv()
+    tg_chat_id = os.environ['TG_CHAT_ID']
     tg_token = os.environ['TELEGRAM_TOKEN']
     bot = telegram.Bot(token=tg_token)
-    bot.send_document(chat_id="@spacebot1234",
-                      document=open('images/spacex0.jpg', 'rb'))
     while True:
         files = listdir("images")
         random.shuffle(files)
         for file in files:
             path = os.path.join("images", file)
             with open(path, "rb") as f:
-                bot.send_document(chat_id="@spacebot1234", document=f)
+                bot.send_document(chat_id=tg_chat_id, document=f)
             sleep(sleep)
 
 
